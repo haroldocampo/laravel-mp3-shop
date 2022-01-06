3f22dbfa425896acfffb10c83bb3c69908f15257
Har <haroldocampo@live.com>
2022-01-06 15:49:30 +0800

    CSS polish

--- src/react/src/App.js
+++ src/react/src/App.js
@@ -117,39 +117,55 @@ class App extends Component {
     window.soundplayer.play();
   }
 
+  removeCart(item) {
+    const newCart = this.state.cart.filter((i) => i.id !== item.id);
+
+    this.setState({cart: newCart});
+  }
+
   render() {
     const { error, isLoaded, songs, cart, library, user } = this.state;
     return (
       <div>
-        <h2>Dotty MP3 Store</h2>
-        <h3>Welcome {user.name}</h3>
-        <ul>
-          {songs.map((item) => (
-            <li key={item.id}>
-              {item.songName} - {item.artist}{" "}
-              <button onClick={() => this.addToCart(item)}>+ Cart</button>
-            </li>
-          ))}
-        </ul>
-        <h2>Your Shopping Cart</h2>
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              {item.songName} - {item.artist}{" "}
-              <button onClick={() => this.removeCart(item)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-        <button onClick={() => this.checkout()}>Checkout</button>
-        <h2>Your Song Library</h2>
-        <ul>
-          {library.map((item) => (
-            <li key={item.id}>
-              {item.songName} - {item.artist}{" "}
-              <button onClick={() => this.playAudio(item)}>Play</button>
-            </li>
-          ))}
-        </ul>
+        <b>Welcome {user.name} <small>({user.email})</small></b>
+        <div className="row">
+          <div className="col-md-6">
+            <h2>Dotty MP3 Store</h2>
+            <ul>
+              {songs.map((item) => (
+                <li key={item.id}>
+                  {item.songName} - {item.artist}{" "}
+                  <button className="right" onClick={() => this.addToCart(item)}>+ Cart</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-md-6 relative cart">
+            <h2>Your Shopping Cart</h2>
+            <ul>
+              {cart.map((item) => (
+                <li key={item.id}>
+                  {item.songName} - {item.artist}{" "}
+                  <button className="right" onClick={() => this.removeCart(item)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+            <button className="checkout btn" disabled={!cart.length} onClick={() => this.checkout()}>Checkout</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 center">
+            <h2>Your Song Library</h2>
+            <ul>
+              {library.map((item) => (
+                <li key={item.id}>
+                  {item.songName} - {item.artist}{" "}
+                  <button className="right" onClick={() => this.playAudio(item)}>Play</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
